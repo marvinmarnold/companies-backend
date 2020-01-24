eb00c344d59de6229eaa72e82d2078d6b83073f0
Marvin Arnold <ppad@geeky.rocks>
2020-01-24 10:25:38 -0600

    Create tree structure JSON of all entries

--- index.js
+++ index.js
@@ -47,34 +47,67 @@ const organizeCompaniesByFirstLetter = companyNames => {
 	})
 }
 
+const organizeChildrenIntoMaxSize = parentNode => {
+	console.log("Organizing nodes for " + parentNode.prefix + " with " + parentNode.children.length + " entries")
+
+	const numEntries = parentNode.children.length
+	if (numEntries <= pageLimit) return
+	// create numBuckets = min
+
+	const numEntriesPerBucket = Math.ceil(numEntries / pageLimit)
+	console.log("Each bucket has " + numEntriesPerBucket)
+	const batchedEntries = _.chunk(parentNode.children, numEntriesPerBucket)
+
+	// reset the parent node's children
+	parentNode.children = []
+	// create a new node for each chunk 
+	let i = 1
+	_.each(batchedEntries, batch => {
+		const newNode = {
+			type: NAV_TYPE,
+			prefix: "-" + i,
+			parent: parentNode,
+			children: batch
+		}
+
+		parentNode.children.push(newNode)
+		organizeChildrenIntoMaxSize(newNode)
+
+		i += 1
+	})
+}
+
 // group companies by first letter
 // if the number of companies is greater than limit, bucket
 const createTree = (companyNames) => {
 	console.log("Creating tree structure") 
 	// first organize nodes by first letter
 	organizeCompaniesByFirstLetter(companyNames)
-	console.log(rootNode.children.length)
-	console.log(rootNode.children[0].children.length)
-	console.log(rootNode.children[1].children.length)
+	_.each(rootNode.children, letterNode => {
+		console.log(letterNode.prefix + ": " + letterNode.children.length)
+	})
 
 	// then organize all children correctly for each letter
-	// rootNode = organizeChildren(rootNode)
+	_.each(rootNode.children, letterBucket => {
+		organizeChildrenIntoMaxSize(letterBucket)
+	})
 
 	// write to JSON
-	// writeTreeToJson(rootNode)
+	writeTreeToJson(rootNode)
 }
 
 // write full tree to JSON file
 const writeTreeToJson = tree => {
 	console.log("Writing tree to JSON file")
-	console.log(tree.children.length)
+	console.log(tree.children[0].children.length)
+	console.log(tree)
 }
 
 // fetch companies from https://seo-interview.herokuapp.com/companies
 const fetchCompanies = () => {
 	request(apiUrl, function (error, response, body) {
-	  console.log('error:', error); // Print the error if one occurred
-	  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+	  // console.log('error:', error); // Print the error if one occurred
+	  // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
 	  // console.log('body:', body); // Print the HTML for the Google homepage.
 
 	  let companyNames = JSON.parse(body).companies
@@ -89,5 +122,3 @@ const fetchCompanies = () => {
 // Start processing
 fetchCompanies()
 
-// test that it works
-// make sure in alphabetical order
